e1945a579a567993b8a7c768a3265b47838a7a90
ZoyaFaisalSiddiqui <zoya.fsiddiqui06@gmail.com>
2026-06-28 17:43:53 +0500

    customer login fixed

--- server/routes/customer.py
+++ server/routes/customer.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from config.db import customer_collection
-from model.customer_model import CustomerRegister
+from model.customer_model import CustomerRegister, CustomerLogin
 
 router = APIRouter(prefix="/customer", tags=["Customer"])
 
@@ -26,4 +26,35 @@ def register_customer(customer: CustomerRegister):
     return {
         "success": True,
         "message": "Customer Registered Successfully"
+    }
+
+@router.post("/login")
+def login_customer(customer: CustomerLogin):
+
+    existing = customer_collection.find_one(
+        {
+            "email": customer.email
+        }
+    )
+
+    if not existing:
+        return {
+            "success": False,
+            "message": "Email not found"
+        }
+
+    if existing["password"] != customer.password:
+        return {
+            "success": False,
+            "message": "Incorrect password"
+        }
+
+    return {
+        "success": True,
+        "message": "Login Successful",
+        "user": {
+            "id": str(existing["_id"]),
+            "fullName": existing["fullName"],
+            "email": existing["email"]
+        }
     }
