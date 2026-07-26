e72ccac47a096d63966c525235a420db1947ee04
Zohaib Ali <zohaibaliwork@gmail.com>
2026-07-26 21:59:07 +0500

    memory issue removed

--- server/config/gemini.py
+++ server/config/gemini.py
@@ -1,12 +1,7 @@
 import os
 from dotenv import load_dotenv
-import google.generativeai as genai
 
 load_dotenv()
-
-genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
 
 MODEL_NAME = os.getenv(
     "GEMINI_MODEL",
